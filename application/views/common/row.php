016a3502cc6ba6faca76e386ff310a6a82550b23
cherlaj <36751085+cherlaj@users.noreply.github.com>
2018-04-05 11:08:10 +0800

    emergency report action

--- application/views/common/row.php
+++ application/views/common/row.php
@@ -129,6 +129,63 @@
         });
         
     });
+
+    $(document).on("click", ".view-map", function(){
+        var url = $(this).data("href");
+        var id = $(this).data("id");
+
+    
+        $.ajax({
+            url : url ,
+            method : 'get' ,
+            success : function(response){
+                var json = jQuery.parseJSON(response);
+                var modal = $('#viewMap').modal('show');
+                modal.find("tbody").html(" ");
+                var longitude = parseFloat(json.result["longitude"]);
+                var latitude = parseFloat(json.result["latitude"]);
+                    
+                var tr = $("<tr>");
+
+                tr.append($("<td>").append(json.result["emergency_id"]));
+                tr.append($("<td>").append(json.result["name"] + " " + json.result["surname"]));
+                tr.append($("<td>").append(json.result["longitude"]));
+                tr.append($("<td>").append(json.result["latitude"]));
+                tr.append($("<td>").append(json.result["comment"]));
+                tr.append($("<td>").append(json.result["status"]));
+
+                modal.find("tbody").append(tr);
+                $('#map-tab').tab('show');
+
+                modal.find(".report-images").html(" ");
+                $.each(json.result["images"] , function(k , v){
+                    console.log(v.images);
+                    var div = $(".report-images");
+
+                    div.append($("<a class='col-lg-3 col-md-4 col-sm-6 col-xs-12' href='<?php echo site_url('public/upload/emergency/'); ?>"+v.images+"' data-sub-html='Emergency # "+v.emergency_id+"'><img class='img-responsive thumbnail' src='<?php echo site_url('public/upload/emergency/'); ?>"+v.images+"'></a>" ));
+
+                    modal.find(".report-images").append(div);
+                });
+
+                setTimeout(function(){
+                    var uluru = {lat: latitude, lng: longitude};
+                    var map = new google.maps.Map(document.getElementById('_map'), {
+                      zoom: 14,
+                      center: uluru
+                    });
+
+                    var marker = new google.maps.Marker({
+                      position: uluru,
+                      map: map
+                    });
+
+                   
+                },500);
+
+                modal.find('#map_emergency_id').text(id);
+            }
+        });
+    });
 </script>
 
 <div class="row clearfix">
@@ -371,7 +428,6 @@
                                     <th><nobr>Driver</nobr></th>
                                     <th><nobr>Comment</nobr></th>
                                     <th><nobr>Status</nobr></th>
-                                    <th><nobr>Latitude</nobr></th>
                                     <th><nobr>Created</nobr></th>
                                     <th>Action</th>
                                 </tr>
@@ -384,8 +440,7 @@
                                         <td><?php echo $row->comment; ?></td>
                                         <td><?php echo $row->status; ?></td>
                                         <td><?php echo $row->created; ?></td>
-                                        <td><?php echo ($row->fix_date == '0') ? "NA" : $row->fix_date; ?></td>
-                                        <td><a href="javascript:void(0);" data-id="<?php echo $row->emergency_id; ?>" data-href="<?php echo site_url('app/mechanic/update_emergency_report/'.$row->emergency_id) ?>" class="btn btn-xs btn-info viewModal">Fix</a></td>
+                                        <td><a href="javascript:void(0);" class="btn btn-primary btn-xs view-map" data-id="<?php echo $row->emergency_id; ?>" data-href="<?php echo site_url('app/mechanic/view_emergency_report/').$row->emergency_id; ?>"><i class="material-icons" style="font-size: 16.5px;">my_location</i> View Report</a></td>
                                     </tr>
                                 <?php endforeach; ?>
                             </tbody>
@@ -514,3 +569,56 @@
 <!-- END COLLAPSE TABLE -->
     </div>
 </div>
+
+<!-- MODAL VIEW -->
+
+<div class="modal fade" id="viewMap" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content ">
+            <div class="modal-header">
+                <h4 class="modal-title">Emergency Report # <span id="map_emergency_id"></span></h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <table class="table table-striped table-hover table-bordered dt">
+                        <thead>
+                            <tr>
+                                <td>Emergency #</td>
+                                <td>Driver</td>
+                                <td>Longitude</td>
+                                <td>Latitude</td>
+                                <td>Comment</td>
+                                <td>Status</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>                        
+                    </table>
+                </div>
+
+                <ul class="nav nav-tabs">
+                    <li class="active"><a id="map-tab" data-toggle="tab" href="#_map">Map</a></li>
+                    <li><a data-toggle="tab" href="#emergencyreport-image">Images</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div id="emergencyreport-image" class="tab-pane fade" style="margin-bottom: 20px;">
+                        <div class="list-unstyled clearfix">
+                            <div class="row report-images">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div id="_map" class="tab-pane fade in active" style="width:100%;height:400px">
+                        
+                    </div>
+                </div>
+                
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger waves-effect" data-dismiss="modal">CLOSE</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- #END# Basic Examples -->
